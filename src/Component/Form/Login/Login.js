a3c4aa81585103d35c86f396f26cb70261304017
hamzaali <hamza54834@gmail.com>
2021-02-11 23:48:40 +0500

    authentication complete

--- src/Component/Form/Login/Login.js
+++ src/Component/Form/Login/Login.js
@@ -1,17 +1,51 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../../features/userSlice'
+import {db,storage,auth} from '../../../firebase'
 import './login.css';
+
+console.log(login);
+
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name,setName] = useState("");
     const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+    console.log(dispatch);
     const loginToApp = (e)=> {
         e.preventDefault();
     };
 
     const register = () => {
-    
+    if(!name){
+        return alert("Please enter a full name!")
+    }
+
+    auth.createUserWithEmailAndPassword(email, password)
+         .then((userAuth)=> {
+                console.log('authUser',userAuth);
+                // authUser.user
+              return userAuth.user.updateProfile({
+                  displayName: name,
+                  photoURL: profilePic
+                })
+                .then(() => {
+                dispatch(
+                    login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoURL: profilePic,
+                })
+
+                )
+                })
+         })
+         .catch(function (error) {
+           alert(error.message)
+         });
     };
 
     return (
