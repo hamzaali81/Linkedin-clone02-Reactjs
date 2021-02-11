e180b8a2d9811720dd98dcf21c7c528202b6db79
hamzaali <hamza54834@gmail.com>
2021-02-11 11:46:34 +0500

    Redux/toolkit implementation in project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import Header from './Component/Header/Header';
 import SideBar from "./Component/Sidebar/Sidebar";
 import Feed from "./Component/Feed/Feed";
-
+import Login from './Component/Form/Login';
 import './App.css';
 
+import { selectUser } from './features/userSlice';
+import { useSelector } from "react-redux";
+
 function App() {
+const user = useSelector(selectUser);
+console.log(user);
   return (
     <div className="app">
     
      {/* Header */}
     <Header />
-
-     {/* App Body  */}
+    {
+      !user ? <Login /> :
+      //  {/* App Body  */}
      <div className="app__body">
      {/* Sidebar */}
        <SideBar />
@@ -19,6 +26,8 @@ function App() {
      <Feed />
      {/* Widgets */}
      </div>
+    }
+
     </div>
   );
 }
